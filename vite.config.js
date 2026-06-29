7d4172602d89bf061c1aef9bec34fc359739d6a4
shubu001 <shubhamkharwar001@gmail.com>
2026-06-29 21:05:17 +0530

    Fix sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: "https://travelbharat360.vercel.app",
+    }),
+  ],
 });
